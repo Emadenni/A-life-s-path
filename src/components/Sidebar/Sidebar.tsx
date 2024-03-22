3c7b78104a2de270a80a2ffc4bddb170e9f6f8fa
Emadenni <emanyele.dng86@gmail.com>
2024-03-22 23:35:34 +0100

    create navigation among components

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,27 +1,31 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import "./sidebar.scss";
 
-type Props = {};
+type Props = {
+  onSelectComponent: (componentName: string) => void;
+};
 
-const Sidebar = (props: Props) => {
+const Sidebar = ({ onSelectComponent }: Props) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleItemClick = (componentName: string) => {
+    onSelectComponent(componentName); // Comunica aResume quale componente deve essere visualizzato
+  };
+
   return (
     <>
       <div className={`sidebar-wrapper ${isOpen ? 'open' : ''}`}>
       <ul className="sidebar-container">
-        <li onClick={()=>console.log("clicked")
-        }>About Me <span className="label blue"></span> </li> 
-        <li>Education <span className="label red"></span> </li>
-        <li>Experiences <span className="label green"></span> </li>
-        <li>Skills<span className="label orange"></span> </li>
-        <li>Porftolio <span className="label purple"></span> </li>
-        <li>Extra <span className="label brown"></span> </li>
+        <li onClick={() => handleItemClick("AboutMe")}>About Me <span className="label blue"></span> </li> 
+        <li onClick={() => handleItemClick("Education")}>Education <span className="label red"></span> </li>
+        <li onClick={() => handleItemClick("Experiences")}>Experiences <span className="label green"></span> </li>
+        <li onClick={() => handleItemClick("Skills")}>Skills<span className="label orange"></span> </li>
+        <li onClick={() => handleItemClick("Portfolio")}>Porftolio <span className="label purple"></span> </li>
+        <li onClick={() => handleItemClick("Extra")}>Extra <span className="label brown"></span> </li>
       </ul>
 
       <button onClick={toggleSidebar} className="toggleSidebar-button">
--- src/pages/Resume/Resume.tsx
+++ src/pages/Resume/Resume.tsx
@@ -1,26 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./resume.scss";
 import Sidebar from "../../components/Sidebar/Sidebar";
-import Frame from "../../components/Frame/Frame";
 import AboutMe from "../../components/AboutMe/AboutMe";
 import Education from "../../components/Education/Education";
 import Experiences from "../../components/Experiences/Experiences";
 import Skills from "../../components/Skills/Skills";
 import Portfolio from "../../components/Portfolio/Portfolio";
 import Extra from "../../components/Extra/Extra";
-type Props = {};
 
-const Resume = (props: Props) => {
+const Resume = () => {
+  const [selectedComponent, setSelectedComponent] = useState("AboutMe");
+
+  const handleSelectComponent = (componentName: string) => {
+    setSelectedComponent(componentName);
+  };
+
   return (
     <div className="resume-container">
-      <Sidebar />
-       <Frame borderColor="rgba(11, 11, 11, 0.2)" backgroundColor="rgba(11, 11, 11, 0.2)" title="Main Informations" />
-      {/*      <AboutMe /> */}
-      {/* <Education /> */}
-      {/*  <Experiences /> */}
-      {/* <Skills /> */}
-      {/*      <Portfolio /> */}
-    {/*   <Extra /> */}
+      <Sidebar onSelectComponent={handleSelectComponent} />
+      <div className="selected-component">
+        {selectedComponent === "AboutMe" && <AboutMe />}
+        {selectedComponent === "Education" && <Education />}
+        {selectedComponent === "Experiences" && <Experiences />}
+        {selectedComponent === "Skills" && <Skills />}
+        {selectedComponent === "Portfolio" && <Portfolio />}
+        {selectedComponent === "Extra" && <Extra />}
+      </div>
     </div>
   );
 };
