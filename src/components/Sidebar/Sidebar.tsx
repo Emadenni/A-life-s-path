fe4fa30fb754af458521d0f7f9a18ff29aeb3d86
Emadenni <emanyele.dng86@gmail.com>
2024-03-25 11:31:52 +0100

    improved sidebar experience

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./sidebar.scss";
 import useSidebarStore from "../../Store/store";
+import logoALP from "../../assets/images/logoALP.png"
 
 type Props = {
   onSelectComponent: (componentName: string) => void;
@@ -10,24 +11,43 @@ const Sidebar = ({ onSelectComponent }: Props) => {
   const { isOpen, toggleSidebar } = useSidebarStore();
 
   const handleItemClick = (componentName: string) => {
-    onSelectComponent(componentName); 
+    onSelectComponent(componentName);
   };
 
   return (
-    <div className={`sidebar-wrapper ${isOpen ? 'open' : ''}`}>
-      <ul className="sidebar-container">
-        <li onClick={() => handleItemClick("AboutMe")}>About Me <span className="label blue"></span> </li> 
-        <li onClick={() => handleItemClick("Education")}>Education <span className="label red"></span> </li>
-        <li onClick={() => handleItemClick("Experiences")}>Experiences <span className="label green"></span> </li>
-        <li onClick={() => handleItemClick("Skills")}>Skills<span className="label orange"></span> </li>
-        <li onClick={() => handleItemClick("Portfolio")}>Porftolio <span className="label purple"></span> </li>
-        <li onClick={() => handleItemClick("Extra")}>Extra <span className="label brown"></span> </li>
-      </ul>
+    <>
+      {" "}
+      <div className={`sidebar-wrapper ${isOpen ? "open" : ""}`}>
+        <ul className="sidebar-container">
+          <li onClick={() => handleItemClick("AboutMe")}>
+            About Me <span className="label blue"></span>{" "}
+          </li>
+          <li onClick={() => handleItemClick("Education")}>
+            Education <span className="label red"></span>{" "}
+          </li>
+          <li onClick={() => handleItemClick("Experiences")}>
+            Experiences <span className="label green"></span>{" "}
+          </li>
+          <li onClick={() => handleItemClick("Skills")}>
+            Skills<span className="label orange"></span>{" "}
+          </li>
+          <li onClick={() => handleItemClick("Portfolio")}>
+            Porftolio <span className="label purple"></span>{" "}
+          </li>
+          <li onClick={() => handleItemClick("Extra")}>
+            Extra <span className="label brown"></span>{" "}
+          </li>
+        </ul>
 
-      <button onClick={toggleSidebar} className="toggleSidebar-button">
-        {isOpen ? '<<' : '>>'}
-      </button>
-    </div>
+        <button onClick={toggleSidebar} className="toggleSidebar-button">
+          {isOpen ? "<<" : ">>"}
+        </button>
+        <div  className={` ${isOpen ? "overlay" : "hidden"}`}>
+          <img src={logoALP} alt="logo" />
+        </div>
+      </div>
+
+    </>
   );
 };
 
